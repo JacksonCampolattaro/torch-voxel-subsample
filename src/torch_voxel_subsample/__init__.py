9a75cde11189f442b23509709eb22b8757709a25
Jackson Campolattaro <jackson.campolattaro@gmail.com>
2025-02-26 14:12:12 +0100

    Add implementation from DeLA; simple unit tests confirming it works

--- src/torch_voxel_subsample/__init__.py
+++ src/torch_voxel_subsample/__init__.py
@@ -1,5 +1,41 @@
+from pathlib import Path
+
+import torch
 from torch import Tensor
+from torch.utils import cpp_extension
+
+voxel_subsample_cpp = cpp_extension.load(
+    name='cpp_voxel_subsample',
+    sources=[str(Path(__file__).parent / 'voxel_subsample.cpp')],
+    extra_cflags=["-O3", "-mavx2", "-funroll-loops"],
+    # extra_cuda_cflags=["-Xptxas", "-v"]  # not necessary for now
+)
+
+
+def next_prime(x) -> int:
+    r"""
+    Finds the next prime, x included.
+    x should be >= 3 for a correct result.
+    """
+    x = int(x) | 1
+    for i in range(x, 2 * x, 2):
+        prime = True
+        for j in range(3, int(i ** 0.5) + 1, 2):
+            if i % j == 0:
+                prime = False
+                break
+        if prime:
+            return i
 
 
 def voxel_subsample(pos: Tensor, voxel_size: float, hash_size: float = 1.0) -> Tensor:
-    pass
+    assert pos.ndim == 2 and pos.shape[1] == 3 and pos.dtype == torch.float
+    if pos.stride(0) != 3:
+        pos = pos.contiguous()
+
+    size = pos.shape[0] * hash_size
+    size = next_prime(size)
+    table = torch.zeros((size,), dtype=torch.int64)
+    storage = torch.empty((size * 3,), dtype=torch.int64)
+    indices = voxel_subsample_cpp.voxel_subsample(pos, voxel_size, table, storage)
+    return indices
--- tests/test_voxel_subsample.py
+++ tests/test_voxel_subsample.py
@@ -1,12 +1,45 @@
+import math
+import time
+
 import pytest
+import torch
 
 from torch_voxel_subsample import voxel_subsample
 
 
-def test_subsampling():
+def test_subsampling_fully_occupied():
+    num_cells = 4 ** 3
+    voxel_size = 1 / math.cbrt(num_cells)
+
+    # Generate a random dense point cloud
+    pos = torch.rand(10_000, 3)
+
+    # Sample it sparsely (ensuring that every cell will contain a particle)
+    samples = voxel_subsample(pos, voxel_size)
+
+    assert len(samples) == num_cells
+
+
+@pytest.mark.parametrize('n', [100, 1_000, 10_000])
+@pytest.mark.parametrize('voxel_size', [1 / 2, 1 / 8, 1 / 32])
+def test_subsampling_correctness(n: int, voxel_size: float):
     # Generate a random point cloud
-    # todo
+    pos = torch.rand(n, 3)
 
-    # Choose a voxel size
+    # Apply sampling
+    samples = voxel_subsample(pos, voxel_size)
 
-    assert True
+    # todo: check spacing between particles
+
+    num_voxels = (1 / voxel_size) ** 3
+    assert len(samples) <= num_voxels
+
+
+def test_subsampling_speed():
+    pos = torch.rand(1_000_000, 3)
+    start_time = time.time()
+    samples = voxel_subsample(pos, 1 / 1_000)
+    elapsed_time = time.time() - start_time
+    assert len(samples) > 0
+    print(f" ({elapsed_time:.02}s)")
+    assert elapsed_time < 1.0
