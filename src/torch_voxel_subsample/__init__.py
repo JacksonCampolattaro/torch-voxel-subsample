b9e9a02f51e7269d2af5ccfcb854d1af001ca248
Jackson Campolattaro <jackson.campolattaro@gmail.com>
2025-02-26 13:27:45 +0100

    Add pyproject.toml, placeholder code

---
+++ src/torch_voxel_subsample/__init__.py
@@ -0,0 +1,5 @@
+from torch import Tensor
+
+
+def voxel_subsample(pos: Tensor, voxel_size: float, hash_size: float = 1.0) -> Tensor:
+    pass
---
+++ tests/test_voxel_subsample.py
@@ -0,0 +1,12 @@
+import pytest
+
+from torch_voxel_subsample import voxel_subsample
+
+
+def test_subsampling():
+    # Generate a random point cloud
+    # todo
+
+    # Choose a voxel size
+
+    assert True
